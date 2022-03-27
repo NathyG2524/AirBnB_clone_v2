769aaf0e8f5e661f54e5001a4edb5866a0bc29aa
root <nathygirma2524@gmail.com>
2022-03-27 10:13:37 -0700

    pycodestyle

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-"""
-    Fabric script that generates a .tgz archive 
-    from the contents of the web_static folder 
-"""
+"""Fabric script that generates a .tgz archive """
 from fabric.api import local
 from datetime import datetime
 import os
 
-filename = "web_static_" + datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f") + ".tgz"
+filename = "web_static_" + \
+    datetime.now().replace(microsecond=0).strftime("%Y-%m-%d %H:%M:%S.%f")\
+    + ".tgz"
+
 
 def do_pack():
-    """
-    Fabric script
-    """
+    """ Fabric script that creates an archive """
     print("Packing web_static to version/{}".format(filename))
     if not os.path.isdir("versions"):
         os.mkdir("versions")
     local("tar -cvzf versions/{} web_static".format(filename))
-    print("web_static packed: versions/{} -> {}Bytes".format(filename), os.path.getsize("versions/{}".format(filename)))
+    print("web_static packed: versions/{} -> {}Bytes".format(filename),
+          os.path.getsize("versions/{}".format(filename)))
